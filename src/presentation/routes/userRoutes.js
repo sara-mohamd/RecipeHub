8269ffe514b3c6a970cc6c5623c37f5b79b4ed6c
sara-mohamd <saraamohamed0038@gmail.com>
2025-02-11 23:04:29 +0200

    Remove unused authentication-related files and interfaces

--- src/presentation/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const UserController = require('../controllers/UserController');
-
-const router = express.Router();
-router.post('/register', UserController.register);
-router.get('/get', UserController.getUserByUsername);
-router.delete('/delete', UserController.deleteUser)
-module.exports = router;
